df206c4d41e1af025f8a0b9cf9d6e1a165a3d6bd
Tom Duclos <48413394+KCRitoki@users.noreply.github.com>
2022-12-16 08:28:55 +0000

    SHIFUMI SIUUUUUUUUU

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      // Ask the user to choose rock, paper, or scissors
+      System.out.println("Choose rock, paper, or scissors:");
+      String userChoice = scanner.nextLine();
+
+      // Generate a random number to simulate the computer's choice
+      int computerChoice = (int)(Math.random() * 3);
+      String[] choices = {"rock", "paper", "scissors"};
+      String computerChoiceString = choices[computerChoice];
+      System.out.println("Computer chose : " + computerChoiceString);
+
+      // Compare the user's choice to the computer s choice to determine the winner
+      if (userChoice.equals(computerChoiceString)) {
+        System.out.println("It's a tie!");
+      } else if (
+        (userChoice.equals("rock") && computerChoiceString.equals("scissors")) ||
+        (userChoice.equals("scissors") && computerChoiceString.equals("paper")) ||
+        (userChoice.equals("paper") && computerChoiceString.equals("rock"))
+      ) {
+        System.out.println("You win!");
+      } else {
+        System.out.println("The computer wins.");
+      }
+    }
+  }
+}
