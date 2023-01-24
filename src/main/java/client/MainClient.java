f3d33b94d2e372d23cb23402a3c562575458a155
KCRitoki <tom.duclos@outlook.fr>
2023-01-24 17:51:57 +0100

    ajout de pseudo (peut etre fini)

--- src/main/java/client/MainClient.java
+++ src/main/java/client/MainClient.java
@@ -1,14 +1,18 @@
 package client;
 
+import java.util.Scanner;
+
 public class MainClient {
 	/** * construct a new client * @param args */
 	public static void main(String[] args) {
 		if (args.length != 3) {
 			printUsage();
 		} else {
+			Scanner scPseudo = new Scanner(System.in);
+			System.out.print("Votre pseudo >> ");
+			String pseudo = scPseudo.nextLine();
 			String address = args[0];
 			Integer port = Integer.parseInt(args[1]);
-			String pseudo = args[2];
 			Client c = new Client(address, port, pseudo);
 		}
 	}
