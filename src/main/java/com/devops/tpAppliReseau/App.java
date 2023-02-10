3bc8a0b333f30c131dfd85997f7e6b27be3ebc75
inesb <bourhim.ines@gmail.com>
2023-02-10 10:23:07 +0100

    Interface graphique : bouton start et action sur le bouton

--- src/main/java/com/devops/tpAppliReseau/App.java
+++ src/main/java/com/devops/tpAppliReseau/App.java
@@ -1,29 +1,16 @@
 package com.devops.tpAppliReseau;
 
-import javax.swing.*;
+import javafx.application.Application;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static ShuFuMi scene;
+public class App {
+    public static ShiFuMi scene;
     public static void main( String[] args )
     {
-        //Fenêtre du jeu
-        JFrame fenetre = new JFrame("Shifumi.cit");
-        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenetre.setSize(980,590);
-        fenetre.setLocationRelativeTo(null);
-        fenetre.setResizable(false);
-        fenetre.setAlwaysOnTop(true);
-
-        //Instanciation de l'objet scene
-        scene = new ShuFuMi();
-
-        fenetre.setContentPane(scene); //On associe la scene à la fenêtre du jeu
-        fenetre.setVisible(true);
+        Application.launch(ShiFuMi.class, args);
 
 
     }
